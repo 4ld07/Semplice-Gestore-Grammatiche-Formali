8a5eaac95a80ac02443a9aee7c525e192399631c
4ld07 <aldotoschi.toschi@gmail.com>
2025-04-28 09:09:02 +0200

    ADD derivation tree plus some fixes

--- grammatiche.py
+++ grammatiche.py
@@ -1,87 +1,149 @@
+import random
+
+class Nodo:
+    def __init__(self, valore, profondità = 0):
+        self.valore = valore
+        self.profondita = profondità
+        self.figli = []
+
+    def aggiungi_figlio(self, figlio):
+        self.figli.append(figlio)
+
+def stampa_albero(nodo, livello=0):
+    print(" " * livello * 2 + str(nodo.profondita) +" "+ str(nodo.valore))
+    for figlio in nodo.figli:
+        stampa_albero(figlio, livello + 1)
+
 class Grammatica:
     def __init__(self):
-        self.non_terminali = set() # array dei simboli non terminali
-        self.terminali = set() # array dei simboli terminali
-        self.regole = {
-                    """
-                        dizionario in cui:
-                        chiavi -> simboli non terminali
-                        valori -> tuple di possibili produzioni (terminali o non)
-                    """
-                } 
+        self.non_terminali = set()  # array dei simboli non terminali
+        self.terminali = set()  # array dei simboli terminali
+        self.regole = dict()  # dizionario
         self.simbolo_iniziale = None
+        self.radice = None
 
-    def add_nt (self, simbolo): # aggiunge simboli non terminali
+    def add_nt(self, simbolo):  # aggiunge simboli non terminali
         if simbolo in self.terminali:
             raise ValueError(f"{simbolo} è un simbolo terminale")
 
-        if not isinistance(simbolo, str):
+        if not isinstance(simbolo, str):
             raise TypeError("Devi inserire una stringa")
 
-        
         self.non_terminali.add(simbolo)
 
-
-    def add_t (self, simbolo): # aggiunge simboli terminali
+    def add_t(self, simbolo):  # aggiunge simboli terminali
         if simbolo in self.non_terminali:
-            raise ValueError(f"{simbolo} è un simbolo non terminale")
+            raise ValueError(f"{simbolo} C( un simbolo non terminale")
 
-        if not isinistance(simbolo, str):
+        if not isinstance(simbolo, str):
             raise TypeError("Devi inserire una stringa")
-        
+
         self.terminali.add(simbolo)
 
-    def set_ini (self, simbolo): # definisce il simbolo iniziale
-        if simbolo not in non_terminali:
+    def set_ini(self, simbolo):  # definisce il simbolo iniziale
+        if simbolo not in self.non_terminali:
             raise ValueError("Deve essere un non terminale")
-            
+
         self.simbolo_iniziale = simbolo
-        
- """dubbio su come verificare che la grammatica funzioni (arrivi ad un risultato in un tempo finito)
-    controllo in creazione regole:
-    imporre almeno un simbolo terminale per ogni produzione/almeno in una produzione?
-    
-    controllo in generazione stringa:
-    limite su iterazioni (es. 20) dopodiche consideriamo le regole non funzionanti?
-    massimo lunghezza risultato dopodiche consideriamo le regole non funzionanti?"""
-    def add_regole (self, chiave, valore): # aggiunge una regola
-        if not isinistance(chiave, str) or valore not isinistance(valore, tuple) or len(valore) < 1:
-            raise ValueError("Argomento non valido") #da rivedere
-        
+
+    def add_regole(self, chiave, valore):  # aggiunge una regola
+        if (
+            not isinstance(chiave, str)
+            or not isinstance(valore, tuple)
+            or len(valore) < 1
+        ):
+            raise ValueError("Argomento non valido")  # da rivedere
+
         self.regole[chiave] = valore
-    
-    def genera_stringa(self):
-        # import random per scegliere con quale simbolo procedere?
-        ris = [self.simbolo_iniziale]
-        ctrl = True
-        while ctrl:
-            for i range(len(ris)):
-                if ris[i] in non_terminali:
-                    ris[i] = list(random.choice(regole[i]))
-           #tutto questo while non funge
-           #ris sarà sempre una lista di un solo elemento
-           #bisogna lavorare su una stringa usando replace()
-           tmp = 0
+
+    def genera_stringa(self, maxi=10):
+
+        ris = self.simbolo_iniziale  # str
+        i = 0
+        while any(simbolo in self.non_terminali for simbolo in ris) and i < maxi:
             for elem in ris:
-                if elem in non_terminali:
-                    tmp += 1
-            ctrl = bool(tmp)
+                if elem in self.non_terminali:
+                    ris = ris.replace(elem, random.choice(self.regole[elem]))
+        i += 1
+
         return ris
-    
-    def genera_nIterazioni(self, n):
-        pnt = self.simbolo_iniziale # puntatore settato al primo elemento
 
-        # lista di stringhe
-        lst_tot = []
-        lst_ris = []
+    # sfida: generare albero di derivazione
+    def genera_albero_derivazione(self, n = 4, profondità = 0, figlio = 0, pnt = None, base = "nodo"):
+        while profondità < n:
+            if pnt == None:
+                # nome di variabile dinamico, perso 2 ore solo per questo 
+                #(locals() dizionario di variabili locali, globals() globali)
+                globals()['%s%d_%d' % (base,profondità,figlio)] = Nodo(self.simbolo_iniziale, profondità)
+                pnt = globals()['%s%d_%d' % (base,profondità,figlio)]
+            else:
+                profondità = pnt.profondita + 1 
+                #calcolare e memorizzare tutte le derivazione diventa 
+                #progressivamente più complicato 
+                #all'aumentare dei simboli non terminali
+                #per guadagnare salute mentale (poca) potremmo cambiare profondità 
+                #ad ogni non terminale incontrato e quindi avere una serie
+                #di sostituzioni parziali fino ad una certa profondità
+                tmp = pnt.valore
+                j = 0 
+                for char in tmp:
+                    if char in self.non_terminali:
+                        prod = set(self.regole[char])
+                        i = 0
+                        tmp_set = set()
+                        for elem in prod:
+                            tmp = pnt.valore.replace(char, elem)
+                            if not tmp_set or tmp not in tmp_set:
+                                tmp_set.add(tmp)
+                                globals()['%s%d_%d' % (base,profondità,i+j)] = Nodo(tmp, profondità)
+                                new = globals()['%s%d_%d' % (base,profondità,i+j)]
+                                pnt.aggiungi_figlio(new)
+                            
+                                i += 1
+                        j += 1 
+                
+                if pnt.figli:
+                    for i in range(len(pnt.figli)):
+                        self.genera_albero_derivazione(n, profondità, i, pnt.figli[i])
+                else:
+                    break
+        
+        return pnt #ritorno la radice alla fine se il cosmo si è allineato perfettamente
 
-        for j in range(len(self.regole[pnt])):
-            tmp = self.regole[pnt][j]
+    def genera_nIterazioni_random(self, n = 4):
+        ris = self.simbolo_iniziale
+        count = 0
 
-            # come cazzo faccio?????
-            # replace() per sostituire i termini all'interno tmp (stringa)
-            # ma come faccio a fare tutte le sostituzione possibili * n volte?
-
-            lst_tot.append(tmp)
+        while count < n - 1:
+            for elem in ris:
+                if elem in self.non_terminali:
+                    choice = []
+                    for i in self.regole[elem]:
+                        if any(a in self.non_terminali for a in i):
+                            choice.append(i)
+                    ris = ris.replace(elem, random.choice(choice))
+            count += 1
+        for elem in ris:
+            if elem in self.non_terminali:
+                choice2 = []
+                for i in self.regole[elem]:
+                    if all(a not in self.non_terminali for a in i):
+                        choice2.append(i)
+                ris = ris.replace(elem, random.choice(choice2))
+        return ris
 
     def __str__(self):
+        pass
+
+
+g = Grammatica()
+g.add_nt("P")
+g.add_t("a")
+g.add_t("b")
+g.set_ini("P")
+g.add_regole("P", ("a", "b", "aPa", "bPb"))
+print(g.genera_stringa())
+
+print(g.genera_nIterazioni_random(15))
+a = g.genera_albero_derivazione(3)
+stampa_albero(a)
